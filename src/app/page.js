af1a077e61f9733377ab76a8926daff1f848bf36
rasty <hvozdetskiyyrii@gmail.com>
2023-07-01 22:41:22 +0300

    disabled

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,13 @@ export default function Home() {
                <span>button</span>
                <span className={'w-5 h-5 bg-amber-300 group-hover:animate-spin transition block'}></span>
             </button>
+            {/*TODO disabled hover*/}
+            <button
+               className={'px-2 py-2 rounded-lg  bg-sky-400 transition hover:opacity-80 disabled:bg-sky-400 hover:disabled:opacity-100'}
+               disabled
+            >
+               button disabled
+            </button>
             {/*TODO :after в класі (gradient)*/}
             <section className={'bg-gray-300 max-w-[1430px] w-full px-[15px] gradient'}>
                {/*TODO завжди буде columns-n колонки*/}
