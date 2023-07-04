8eb7fa7402195eed9bd02a87713d3e193cf04981
rasty <hvozdetskiyyrii@gmail.com>
2023-07-04 19:25:59 +0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -14,9 +14,22 @@ export default function Home() {
                <span>button</span>
                <span className={'w-5 h-5 bg-amber-300 group-hover:animate-spin transition block'}></span>
             </button>
+            {/*TODO задаєм group/name по якому буде працювати ховер*/}
+            <div className={'flex group/button'}>
+               <button className={'bg-green-400 group-hover/button:bg-orange-500 transition'}>
+                  {' '}
+                  with group-hover/name
+               </button>
+               <button className={'bg-green-400 group-hover:bg-violet-500 transition'}>
+                  {' '}
+                  without group-hover/name
+               </button>
+            </div>
             {/*TODO disabled hover*/}
             <button
-               className={'px-2 py-2 rounded-lg  bg-sky-400 transition hover:opacity-80 disabled:bg-sky-400 hover:disabled:opacity-100'}
+               className={
+                  'px-2 py-2 rounded-lg  bg-sky-400 transition hover:opacity-80 disabled:bg-sky-400 hover:disabled:opacity-100'
+               }
                disabled
             >
                button disabled
@@ -52,7 +65,7 @@ export default function Home() {
                   </li>
                </ul>
                {/*TODO divide-y, divide-x - задасть бордер всім дітям крім останього*/}
-               <div className="grid grid-cols-3 divide-x text-center">
+               <div className="grid grid-cols-3 text-center divide-x">
                   <div>01</div>
                   <div>02</div>
                   <div>03</div>
@@ -212,7 +225,7 @@ export default function Home() {
                </p>
                {/*TODO lg:mt-0 lg:flex-col @media потрібно задавати кожній властивості окремо*/}
                {/*TODO hover:text-primary hover:underline decoration-[6px] потрібно задавати кожній властивості окремо*/}
-               <div className={'container flex mt-[10px] lg:mt-0 lg:flex-col'}>
+               <div className={'container flex mt-[10px] lg:mt-0 lg:flex-col text-gray-600'}>
                   <a
                      className={
                         'text-secondary w-[50%] font-bold hover:text-primary hover:underline decoration-[6px] underline-offset-4 transition'
