d3a0431dc9fc8ce68e7b61ca43fa6f2aaaa6a995
rasty <hvozdetskiyyrii@gmail.com>
2024-02-17 19:07:01 +0200

    update

--- src/app/shadcn-ui/page.jsx
+++ src/app/shadcn-ui/page.jsx
@@ -1,5 +1,12 @@
 import {Calendar} from '@/components/ui/Calendar';
 import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from '@/components/ui/Accordion';
+import {
+   Pagination,
+   PaginationContent, PaginationEllipsis,
+   PaginationItem,
+   PaginationLink, PaginationNext,
+   PaginationPrevious
+} from '@/components/ui/pagination';
 
 const ShadcnUiPage = () => {
 
@@ -20,6 +27,30 @@ const ShadcnUiPage = () => {
                </AccordionContent>
             </AccordionItem>
          </Accordion>
+         <Pagination>
+            <PaginationContent>
+               <PaginationItem>
+                  <PaginationPrevious href="#"/>
+               </PaginationItem>
+               <PaginationItem>
+                  <PaginationLink href="#">1</PaginationLink>
+               </PaginationItem>
+               <PaginationItem>
+                  <PaginationLink href="#" isActive>
+                     2
+                  </PaginationLink>
+               </PaginationItem>
+               <PaginationItem>
+                  <PaginationLink href="#">3</PaginationLink>
+               </PaginationItem>
+               <PaginationItem>
+                  <PaginationEllipsis/>
+               </PaginationItem>
+               <PaginationItem>
+                  <PaginationNext href="#"/>
+               </PaginationItem>
+            </PaginationContent>
+         </Pagination>
       </div>
    )
 }
